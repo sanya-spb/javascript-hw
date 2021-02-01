039617e03a9cbec63297c92d2d65772ff35545fc
Alexander Kupriyanov <sanya-spb@mail.ru>
2021-02-01 09:16:57 +0300

    fix1

--- 2.js
+++ 2.js
@@ -3,37 +3,37 @@
 if (!("a" in window)) {
     var a = 1;
 }
+alert(a);
 /*
 
 */
 
-alert(a);
 var b = function a(x) {
     x && a(--x);
 };
+alert(a);
 /*
 
 */
 
-alert(a);
 function a(x) {
     return x * 2;
 }
+var a;
+alert(a);
 /*
 
 */
 
-var a;
-alert(a);
 function b(x, y, a) {
     arguments[2] = 10;
     alert(a);
 }
+b(1, 2, 3);
 /*
 
 */
 
-b(1, 2, 3);
 function a() {
     alert(this);
 }
